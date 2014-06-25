82337bc503b40e61d2b1a1239e0e9de05f6f36a8
Maple Liu <fjliufeng@gmail.com>
2014-06-25 18:52:56 +0800

    Some update.

--- djwp/settings.py
+++ djwp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'oauth2app',
+    'base',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -66,7 +67,7 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-cn'
 
 TIME_ZONE = 'Asia/Chongqing'
 
