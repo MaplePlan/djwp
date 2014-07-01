db53351ae1d4b741ffed053bd98d05fb409f3bb7
Maple Liu <fjliufeng@gmail.com>
2014-07-01 16:02:29 +0800

    Do some update.

---
+++ utils/move_db.py
@@ -0,0 +1,19 @@
+#coding=utf-8
+
+db_map = {
+	'wp_commentmeta':[],
+	'wp_comments':[],
+	'wp_links':[],
+	'wp_options':[],
+	'wp_postmeta':[],
+	'wp_posts':[],
+	'wp_terms':[],
+	'wp_term_relationships':[],
+	'wp_term_taxonomy':[],
+	'wp_users':[],
+	'wp_usermeta':[],
+}
+
+
+if __name__ == '__main__':
+	pass
