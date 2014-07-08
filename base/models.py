7eee84d2c04d744edb9ad9ed60b8a7947162e92e
Maple Liu <fjliufeng@gmail.com>
2014-07-08 17:55:03 +0800

    Jianrong wp database table column.

--- base/models.py
+++ base/models.py
@@ -5,135 +5,184 @@ from django.db import models
 from datetime import datetime
 
 class Wlink(models.Model):
-	wurl = models.CharField(max_length=255)
-	wname = models.CharField(max_length=255)
-	wimage = models.CharField(max_length=255)
-	wtarget = models.CharField(max_length=25)
-	wdescription = models.CharField(max_length=255)
-	wvisible = models.BooleanField(default=True)
-	wowner = models.BigIntegerField(default=1)
-	wrating = models.IntegerField(default=0)
-	wupdated = models.DateTimeField(default=datetime.now())
-	wrel = models.CharField(max_length=255)
-	wnotes = models.TextField()
-	wrss = models.CharField(max_length=255)
+	wid  = models.AutoField(primary_key=True, db_column='link_id')
+	wurl = models.CharField(max_length=255, db_column='link_url')
+	wname = models.CharField(max_length=255, db_column='link_name')
+	wimage = models.CharField(max_length=255, db_column='link_image')
+	wtarget = models.CharField(max_length=25, db_column='link_target')
+	wdescription = models.CharField(max_length=255, db_column='link_description')
+	wvisible = models.CharField(max_length='20', default='Y', db_column='link_visible')
+	wowner = models.BigIntegerField(default=1, db_column='link_owner')
+	wrating = models.IntegerField(default=0, db_column='link_rating')
+	wupdated = models.DateTimeField(default=datetime.now(), db_column='link_updated')
+	wrel = models.CharField(max_length=255, db_column='link_rel')
+	wnotes = models.TextField(db_column='link_notes')
+	wrss = models.CharField(max_length=255, db_column='link_rss')
+
+	class Meta:
+		db_table = 'wp_links'
 
 	def __unicode__(self):
 		return self.wname
 
 class Woption(models.Model):
-	wname = models.CharField(max_length=64)
-	wvalue = models.TextField()
-	wautoload = models.BooleanField(default=True)
+	wid = models.AutoField(primary_key=True, db_column='option_id')
+	wname = models.CharField(max_length=64, unique=True,db_column='option_name')
+	wvalue = models.TextField(db_column='option_value')
+	wautoload = models.CharField(max_length=20, default='yes',db_column='autoload')
+
+	class Meta:
+		db_table = 'wp_options'
 
 	def __unicode__(self):
 		return self.wname
 
 class Wuser(models.Model):
-	wusername = models.CharField(max_length=60)
-	wpassword = models.CharField(max_length=64)
-	wnickname = models.CharField(max_length=50)
-	wemail = models.CharField(max_length=100)
-	wuserurl = models.CharField(max_length=100)
-	wregisteredtime = models.DateTimeField(default=datetime.now())
-	wactivationkey = models.CharField(max_length=60)
-	wstatus = models.IntegerField(default=0)
-	wdisplayname = models.CharField(max_length=255)
+	wid = models.AutoField(primary_key=True, db_column='ID')
+	wusername = models.CharField(max_length=60, db_column='user_login')
+	wpassword = models.CharField(max_length=64, db_column='user_pass')
+	wnickname = models.CharField(max_length=50, db_column='user_nicename')
+	wemail = models.EmailField(max_length=100, db_column='user_email')
+	wuserurl = models.CharField(max_length=100, db_column='user_url')
+	wregisteredtime = models.DateTimeField(default=datetime.now(), db_column='user_registered')
+	wactivationkey = models.CharField(max_length=60, db_column='user_activation_key')
+	wstatus = models.IntegerField(default=0, db_column='user_status')
+	wdisplayname = models.CharField(max_length=255, db_column='display_name')
+
+	class Meta:
+		db_table = 'wp_users'
 
 	def __unicode__(self):
 		return self.wusername
 
 class Wusermeta(models.Model):
-	wuser = models.ForeignKey(Wuser)
-	wmetakey = models.CharField(max_length=255, default='')
-	wmetavalue = models.TextField(default='')
+	wid = models.AutoField(primary_key=True, db_column='umeta_id')
+	wuser = models.ForeignKey(Wuser, db_column='user_id')
+	wmetakey = models.CharField(max_length=255, db_column='meta_key')
+	wmetavalue = models.TextField(default='', db_column='meta_value')
+
+	class Meta:
+		db_table = 'wp_usermeta'
 
 	def __unicode__(self):
 		return self.wmetakey
 
 class Wterm(models.Model):
-	wname = models.CharField(max_length=200)
-	wslug = models.CharField(max_length=200)
-	wtermgroup = models.BigIntegerField(default=0)
+	wid = models.AutoField(primary_key=True, db_column='term_id')
+	wname = models.CharField(max_length=200, db_column='name')
+	wslug = models.CharField(max_length=200, unique=True, db_column='slug')
+	wtermgroup = models.BigIntegerField(default=0, db_column='term_group')
+
+	class Meta:
+		db_table = 'wp_terms'
 
 	def __unicode__(self):
 		return self.wname
 
 class Wtermtaxonomy(models.Model):
-	wterm = models.ForeignKey(Wterm)
-	wtaxonomy = models.CharField(max_length=32)
-	wdescription = models.TextField()
-	wparent = models.BigIntegerField(default=0)
-	wcount = models.BigIntegerField(default=0)
+	wid = models.AutoField(primary_key=True, db_column='term_taxonomy_id')
+	wterm = models.ForeignKey(Wterm, db_column='term_id')
+	wtaxonomy = models.CharField(max_length=32, db_column='taxonomy')
+	wdescription = models.TextField(db_column='description')
+	wparent = models.BigIntegerField(default=0, db_column='parent')
+	wcount = models.BigIntegerField(default=0, db_column='count')
+
+	class Meta:
+		db_table = 'wp_term_taxonomy'
 
 	def __unicode__(self):
 		return self.wtaxonomy
 
 class Wtermrelationship(models.Model):
-	wtermtaxonomy = models.ForeignKey(Wtermtaxonomy)
-	wtermorder = models.IntegerField(default=0)
+	wid = models.AutoField(primary_key=True, db_column='object_id')
+	wtermtaxonomy = models.ForeignKey(Wtermtaxonomy, db_column='term_taxonomy_id')
+	wtermorder = models.IntegerField(default=0, db_column='term_order')
+
+	class Meta:
+		db_table = 'wp_term_relationships'
 
 	def __unicode__(self):
 		return self.wtermorder
 
 class Wpost(models.Model):
-	wauthor = models.ForeignKey(Wuser)
-	wpostdate = models.DateTimeField(default=datetime.now())
-	wpostdategmt = models.DateTimeField(default=datetime.now())
-	wcontent = models.TextField()
-	wtitle = models.TextField()
-	wexcerpt = models.TextField()
-	wstatus = models.CharField(max_length=20, default='publish')
-	wcommentstatus = models.CharField(max_length=20, default='open')
-	wpingstatus = models.CharField(max_length=20, default='open')
-	wpostpassword = models.CharField(max_length=20)
-	wpostname = models.CharField(max_length=200)
-	wtoping = models.TextField()
-	wpinged = models.TextField()
-	wpostmodified = models.DateTimeField(default=datetime.now())
-	wpostmodifiedgmt = models.DateTimeField(default=datetime.now())
-	wcontentfiltered = models.TextField()
-	wpostparent = models.ForeignKey('self', default=0)
-	wguid = models.CharField(max_length=255)
-	wmenuorder = models.IntegerField(default=0)
-	wposttype = models.CharField(max_length=20)
-	wpostmimetype = models.CharField(max_length=100)
-	wcommentcount = models.BigIntegerField(default=0)
+	wid = models.AutoField(primary_key=True, db_column='ID')
+	wauthor = models.ForeignKey('self', db_column='post_author')
+	wpostdate = models.DateTimeField(default=datetime.now(), db_column='post_date')
+	wpostdategmt = models.DateTimeField(default=datetime.now(), db_column='post_date_gmt')
+	wcontent = models.TextField(db_column='post_content')
+	wtitle = models.TextField(db_column='post_title')
+	wexcerpt = models.TextField(db_column='post_excerpt')
+	wstatus = models.CharField(max_length=20, default='publish', db_column='post_status')
+	wcommentstatus = models.CharField(max_length=20, default='open', db_column='comment_status')
+	wpingstatus = models.CharField(max_length=20, default='open', db_column='ping_status')
+	wpostpassword = models.CharField(max_length=20, db_column='post_password')
+	wpostname = models.CharField(max_length=200, db_column='post_name')
+	wtoping = models.TextField(db_column='to_ping')
+	wpinged = models.TextField(db_column='pinged')
+	wpostmodified = models.DateTimeField(default=datetime.now(), db_column='post_modified')
+	wpostmodifiedgmt = models.DateTimeField(default=datetime.now(), db_column='post_modified_gmt')
+	wcontentfiltered = models.TextField(db_column='post_content_filtered')
+	wpostparent = models.BigIntegerField(default=0, db_column='post_parent')
+	wguid = models.CharField(max_length=255, db_column='guid')
+	wmenuorder = models.IntegerField(default=0, db_column='menu_order')
+	wposttype = models.CharField(max_length=20, default='post', db_column='post_type')
+	wpostmimetype = models.CharField(max_length=100, db_column='post_mime_type')
+	wcommentcount = models.BigIntegerField(default=0, db_column='comment_count')
+
+
+	class Meta:
+		db_table = 'wp_posts'
 
 	def __unicode__(self):
 		return self.wtitle
 
 class Wpostmeta(models.Model):
-	wpost = models.ForeignKey(Wpost)
-	wmetakey = models.CharField(max_length=255)
-	wmetavalue = models.TextField()
+	wid = models.AutoField(primary_key=True, db_column='meta_id')
+	wpost = models.ForeignKey(Wpost, db_column='post_id')
+	wmetakey = models.CharField(max_length=255, db_column='meta_key')
+	wmetavalue = models.TextField(db_column='meta_value')
+
+	class Meta:
+		db_table = 'wp_postmeta'
 
 	def __unicode__(self):
 		return self.wmetakey
 
 class Wcomment(models.Model):
-	wpost = models.ForeignKey(Wpost, default=0)
-	wcommentauthor = models.TextField()
-	wcommentauthoremail = models.EmailField(max_length=100)
-	wcommentauthorurl = models.CharField(max_length=200)
-	wcommentauthorip = models.CharField(max_length=100)
-	wcommenttime = models.DateTimeField(default=datetime.now())
-	wcommenttimegmt = models.DateTimeField(default=datetime.now())
-	wcommentcontent = models.TextField()
-	wcommentkarma = models.IntegerField(default=0)
-	wcommentapproved = models.CharField(max_length=20, default='')
-	wcommentagent = models.CharField(max_length=255)
-	wcommenttype = models.CharField(max_length=20)
-	wcommentparent = models.ForeignKey('self', default=0)
-	wuser = models.ForeignKey(Wuser, default=0)
+	wid = models.AutoField(primary_key=True, db_column='comment_ID')
+	wpost = models.ForeignKey(Wpost, default=0, db_column='comment_post_ID')
+	wcommentauthor = models.TextField(db_column='comment_author')
+	wcommentauthoremail = models.EmailField(max_length=100, db_column='comment_author_email')
+	wcommentauthorurl = models.CharField(max_length=200, db_column='comment_author_url')
+	wcommentauthorip = models.CharField(max_length=100, db_column='comment_author_IP')
+	wcommenttime = models.DateTimeField(default=datetime.now(), db_column='comment_date')
+	wcommenttimegmt = models.DateTimeField(default=datetime.now(), db_column='comment_date_gmt')
+	wcommentcontent = models.TextField(db_column='comment_content')
+	wcommentkarma = models.IntegerField(default=0, db_column='comment_karma')
+	wcommentapproved = models.CharField(max_length=20, default='', db_column='comment_approved')
+	wcommentagent = models.CharField(max_length=255, db_column='comment_agent')
+	wcommenttype = models.CharField(max_length=20, db_column='comment_type')
+	wcommentparent = models.ForeignKey('self', default=0, db_column='comment_parent')
+	wuser = models.ForeignKey(Wuser, default=0, db_column='user_id')
+
+	class Meta:
+		db_table = 'wp_comments'
+
 
 	def __unicode__(self):
 		return self.wcommentauthor
 
 class Wcommentmeta(models.Model):
-	wcomment = models.ForeignKey(Wcomment, default=0)
-	wmetakey = models.CharField(max_length=255)
-	wmetavalue = models.TextField()
+	wid = models.AutoField(primary_key=True, db_column='meta_id')
+	wcomment = models.ForeignKey(Wcomment, default=0, db_column='comment_id')
+	wmetakey = models.CharField(max_length=255, db_column='meta_key')
+	wmetavalue = models.TextField(db_column='meta_value')
+
+	class Meta:
+		db_table = 'wp_commentmeta'
+
+	def __unicode__(self):
+		return self.wmetakey
 
 
 
